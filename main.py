b769049aa02af6c3bf12be3439b8823256211c6b
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2023-12-11 20:36:57 +0100

    Add MainWindow class and toolbar to main.py

--- main.py
+++ main.py
@@ -9,5 +9,97 @@
 ********************************************************
 """
 
+import sys
+from PySide6.QtGui import Qt, QCursor
+from PySide6.QtWidgets import (
+    QApplication,
+    QSizePolicy,
+    QVBoxLayout,
+    QHBoxLayout,
+    QSpacerItem,
+    QWidget,
+    QMainWindow,
+    QPushButton,
+    QLabel,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, child: QWidget) -> None:
+        super().__init__()
+
+        self.__widget: QWidget = QWidget(self)
+        layout: QVBoxLayout = QVBoxLayout()
+
+        # Window properties
+        self.setWindowTitle("Maze Solver")
+        self.setCentralWidget(self.__widget)
+        # Widget properties
+        self.__widget.setLayout(layout)
+        # Layout properties
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+        # Add widgets to layout
+        layout.addWidget(self.__toolbar())
+        layout.addWidget(child)
+        # Show window
+        self.show()
+
+    def __toolbar(self) -> QWidget:
+        toolbar: QWidget = QWidget(self.__widget)
+        layout: QHBoxLayout = QHBoxLayout()
+        open: QPushButton = QPushButton("Open", toolbar)
+        reset: QPushButton = QPushButton("Reset", toolbar)
+        save: QPushButton = QPushButton("Save", toolbar)
+        solveDFS: QPushButton = QPushButton("Solve DFS", toolbar)
+        comparison: QPushButton = QPushButton("vs", toolbar)
+        solveBFS: QPushButton = QPushButton("Solve BFS", toolbar)
+
+        # Toolbar properties
+        toolbar.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        toolbar.setLayout(layout)
+        # Open button
+        open.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        open.setShortcut("Ctrl+O")
+        open.clicked.connect(lambda: print("Open"))
+        layout.addWidget(open)
+        # Reset button
+        reset.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        reset.setShortcut("Ctrl+R")
+        reset.clicked.connect(lambda: print("Reset"))
+        layout.addWidget(reset)
+        # Save button
+        save.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        save.setShortcut("Ctrl+S")
+        save.clicked.connect(lambda: print("Save"))
+        layout.addWidget(save)
+        # Spacer
+        layout.addSpacerItem(
+            QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        )
+        # Solve DFS button
+        solveDFS.setDefault(True)
+        solveDFS.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        solveDFS.setShortcut("Ctrl+D")
+        solveDFS.clicked.connect(lambda: print("Solve DFS"))
+        layout.addWidget(solveDFS)
+        # Comparison button
+        comparison.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        comparison.setShortcut("Ctrl+V")
+        comparison.clicked.connect(lambda: print("vs"))
+        layout.addWidget(comparison)
+        # Solve BFS button
+        solveBFS.setDefault(True)
+        solveBFS.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        solveBFS.setShortcut("Ctrl+B")
+        solveBFS.clicked.connect(lambda: print("Solve BFS"))
+        layout.addWidget(solveBFS)
+        return toolbar
+
+
 if __name__ == "__main__":
-    print("Maze Solver")
+    app: QApplication = QApplication(sys.argv)
+    window: MainWindow = MainWindow(QLabel("Maze Board"))
+
+    sys.exit(app.exec())
