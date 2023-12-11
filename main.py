33a43ef052e45ab54ee418733a2ff1c01b74549f
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2023-12-11 14:19:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+********************************************************
+*    Name: Strategies for solution space searching     *
+*    Description: A program that employs algorithms    *
+*                 and artificial intelligence to       *
+*                 solve mazes efficiently.             *
+*    Author: Alejandro Diez Bermejo                    *
+*    Date: December 18, 2023                           *
+********************************************************
+"""
+
+if __name__ == "__main__":
+    print("Maze Solver")
